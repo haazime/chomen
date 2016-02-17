5031647c74c3d9d86027245fa8a28df728e7907f
haazime <h4zime@gmail.com>
2016-02-17 23:07:20 +0900

    destroy Note model

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216140438) do
+ActiveRecord::Schema.define(version: 0) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "notes", force: :cascade do |t|
-    t.text     "content",    null: false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
 end
