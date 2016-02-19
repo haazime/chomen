1d15c278fd2cb979db123bcad75ef33a49319497
haazime <h4zime@gmail.com>
2016-02-20 02:15:07 +0900

    fix autosave

--- app/assets/javascripts/auto_save.js
+++ app/assets/javascripts/auto_save.js
@@ -1,12 +1,19 @@
+var autoSaveTimeoutId;
+var autoSave = function(element) {
+  var form = $(element).closest('form');
+
+  if (autoSaveTimeoutId) clearTimeout(autoSaveTimeoutId);
+  autoSaveTimeoutId = setTimeout(function() {
+    $.rails.handleRemote(form);
+  }, 1500);
+}
+
+var excludeKeyCodes = [9,16,18,17,27,37,38,39,40,91,93];
 var autoSaveHandler = function() {
-  var timeoutId;
-
-  $('textarea[data-auto-save]').on('keypress', function() {
-    var form = $(this).closest('form');
-
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(function() {
-      $.rails.handleRemote(form);
-    }, 500);
+  $('textarea[data-auto-save]').on('keydown', function(e) {
+    var is_skip = jQuery.grep(excludeKeyCodes, function(ex) { return ex == e.keyCode; })
+    if (is_skip.length == 0) {
+      autoSave(this);
+    }
   });
 }
