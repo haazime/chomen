9231d8af8e170b8a061a503db5e6e075846d533c
haazime <h4zime@gmail.com>
2016-02-20 00:16:13 +0900

    fix

--- app/assets/javascripts/auto_save.js
+++ app/assets/javascripts/auto_save.js
@@ -1,12 +1,12 @@
 var autoSaveHandler = function() {
   var timeoutId;
 
-  $('textarea[data-auto-save]').keypress(function() {
+  $('textarea[data-auto-save]').on('keypress', function() {
     var form = $(this).closest('form');
 
     if (timeoutId) clearTimeout(timeoutId);
     timeoutId = setTimeout(function() {
       $.rails.handleRemote(form);
-    }, 1000);
+    }, 500);
   });
 }
