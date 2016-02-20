d217baa9b5263da8ffee8a5f850e89cbd57a53fd
haazime <h4zime@gmail.com>
2016-02-20 16:59:05 +0900

    fix state

--- app/assets/javascripts/flash_state.js
+++ app/assets/javascripts/flash_state.js
@@ -1,11 +1,8 @@
-var stateMessage = '<div class="ui large label"><i class="cloud upload icon"></i> Saved.</div>';
-
 var showState = function() {
-  $(stateMessage)
-    .appendTo('#state')
-    .fadeIn('slow')
+  $('#state')
+    .fadeIn('fast')
     .delay(1000)
     .fadeOut('fast', function() {
-      $(this).remove();
+      $(this).hide();
     });
 }
