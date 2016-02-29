ba5ec3405c17b16ac8ac3a204225e96d048aff68
haazime <h4zime@gmail.com>
2016-02-29 23:10:23 +0900

    fix

--- spec/features/edit_page_spec.rb
+++ spec/features/edit_page_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 describe 'edit page' do
   before { visit edit_page_path(gpid: saved_page.gpid) }
 
-  let(:saved_page) { SavePageCommands::CreatePage.new('CREATE').run }
-  let(:form) { find(css_eid(:chunk, 0)) }
+  let(:saved_page) { SavePageCommands::CreatePage.new('GPID', 'GCID', 'CREATE').run }
+  let(:form) { first('.chunk-form') }
 
   describe 'saved content' do
     let(:content) { form.first('textarea').value }
