0d321d4a8e8f83ed28d43f35ef42800417bdd83c
haazime <h4zime@gmail.com>
2016-06-19 10:53:14 +0900

    indicator component

--- app/assets/javascripts/components/ChunkActions.jsx
+++ app/assets/javascripts/components/ChunkActions.jsx
@@ -1,13 +1,10 @@
 class ChunkActions extends React.Component {
   render() {
-    if (this.props.isSaved) {
-      return (
-        <div className='chunk-actions'>
-          <i className='fa fa-circle-thin' />
-        </div>
-      )
-    } else {
-      return <span />
-    }
+    return (
+      <div className='chunk-actions'>
+        <ChunkRemover />
+        <ChunkIndicator />
+      </div>
+    )
   }
 }
---
+++ app/assets/javascripts/components/ChunkRemover.jsx
@@ -0,0 +1,5 @@
+class ChunkRemover extends React.Component {
+  render() {
+    return <i className='chunk-remover fa fa-circle-thin' />
+  }
+}
