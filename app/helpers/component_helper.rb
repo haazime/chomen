dfd1961bc422d2d7e757159c30526c8a67618ded
haazime <h4zime@gmail.com>
2016-06-19 21:54:00 +0900

    adjust

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -12,7 +12,7 @@ module ComponentHelper
     chunk.attributes.merge(
       gpid: chunk.gpid,
       save_url: pages_url,
-      save_delay: Rails.env == 'test' ? 1 : 2500,
+      save_delay: Rails.env == 'test' ? 1 : 5000,
       destroy_url: chunk.persisted? ? chunk_url(chunk) : nil
     )
   end
