14e4eea684daa060e8ec54478d450febc054a803
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2016-06-21 18:06:03 +0900

    cleanup

--- app/helpers/component_helper.rb
+++ app/helpers/component_helper.rb
@@ -17,21 +17,4 @@ module ComponentHelper
   def save_delay
     @save_delay ||= Rails.env == 'test' ? 1 : 3000
   end
-
-  # TODO: remove
-  #def chunk_list_component_props(chunks)
-  #  {
-  #    active: chunks.max_by(&:updated_at).gcid,
-  #    chunks: chunks.map { |c| chunk_component_props(c) }
-  #  }
-  #end
-
-  #def chunk_component_props(chunk)
-  #  chunk.attributes.merge(
-  #    gpid: chunk.gpid,
-  #    save_url: pages_url,
-  #    save_delay: Rails.env == 'test' ? 1 : 2500,
-  #    destroy_url: chunk.persisted? ? chunk_url(chunk) : nil
-  #  )
-  #end
 end
