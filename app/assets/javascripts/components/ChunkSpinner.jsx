9a5e57ca6ca7676782d0425eaa30101a70955dbf
haazime <h4zime@gmail.com>
2016-06-23 00:57:33 +0900

    fix saving icon size

--- app/assets/javascripts/components/ChunkSpinner.jsx
+++ app/assets/javascripts/components/ChunkSpinner.jsx
@@ -1,3 +1,8 @@
 const ChunkSpinner = () => {
-  return <i className='fa fa-spinner fa-spin chunk-icon chunk-icon-success' />
+  return (
+    <i
+      className='fa fa-spinner fa-spin chunk-icon chunk-icon-success'
+      style={{fontSize: '24px'}}
+    />
+  )
 }
