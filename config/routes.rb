ed3ed609ee49cc73ad7a1cfa6e6c9c43eb6b3862
haazime <h4zime@gmail.com>
2016-06-25 14:43:41 +0900

    delete page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get     '/pages/new',   to: 'pages#new',      as: :new_page
   post    '/pages',       to: 'pages#save',     as: :pages
   get     '/pages/:gpid', to: 'pages#edit',     as: :edit_page
+  delete  '/pages/:gpid', to: 'pages#destroy',  as: :destroy_page
 
   get     '/chunks/new',    to: 'chunks#new',     as: :new_chunk
   delete  '/chunks/:gcid',  to: 'chunks#destroy', as: :destroy_chunk
