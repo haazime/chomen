119bd61d46d624e536d542d9bf6dffada778a589
Hajime Sueyoshi <hajime.sueyoshi@gaiax.com>
2017-03-01 16:20:24 +0900

    fix

--- app/models/page.rb
+++ app/models/page.rb
@@ -25,7 +25,7 @@ class Page < ActiveRecord::Base
   end
 
   def destroy_from_chunk(chunk_id)
-    if chunks(true).many?
+    if chunks.reload.many?
       chunks.destroy(chunk_id)
     else
       destroy
